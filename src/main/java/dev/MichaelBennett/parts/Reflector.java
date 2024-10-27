54e2feed5922f7dd78d5ec4f5ea444468c2e6db8
MGlennUA <michaelbennett2048@gmail.com>
2024-10-26 23:34:22 -0700

    added reflector logic and constructor fix

--- src/main/java/dev/MichaelBennett/parts/Reflector.java
+++ src/main/java/dev/MichaelBennett/parts/Reflector.java
@@ -26,5 +26,23 @@ public class Reflector {
         this.reflectorPairs.put(10,15);
         this.reflectorPairs.put(11,14);
         this.reflectorPairs.put(12,13);
+        this.reflectorPairs.put(13,12);
+        this.reflectorPairs.put(14,11);
+        this.reflectorPairs.put(15,10);
+        this.reflectorPairs.put(16,9);
+        this.reflectorPairs.put(17,8);
+        this.reflectorPairs.put(18,7);
+        this.reflectorPairs.put(19,6);
+        this.reflectorPairs.put(20,5);
+        this.reflectorPairs.put(21,4);
+        this.reflectorPairs.put(22,3);
+        this.reflectorPairs.put(23,2);
+        this.reflectorPairs.put(24,1);
+        this.reflectorPairs.put(25,0);
+    }
+
+
+    public int reflectSignal(int in) {
+        return this.reflectorPairs.get(in);
     }
 }
