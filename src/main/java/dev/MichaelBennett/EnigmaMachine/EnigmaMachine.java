4defa9d6374ec62d205a62189e62c2db705957f7
MGlennUA <michaelbennett2048@gmail.com>
2024-10-27 12:43:09 -0700

    added original enigma encryption rotor values

--- src/main/java/dev/MichaelBennett/EnigmaMachine/EnigmaMachine.java
+++ src/main/java/dev/MichaelBennett/EnigmaMachine/EnigmaMachine.java
@@ -2,6 +2,15 @@ package dev.MichaelBennett.EnigmaMachine;
 
 public class EnigmaMachine {
 
+    public static String rotorIValues = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+    public static String rotorIIValues = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+    public static String rotorIIIValues = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+    public static String rotorIVValues = "ESOVPZJAYQUIRHXLNFTGKDCMWB";
+    public static String rotorVValues = "VZBRGITYUPSDNHLXAWMJQOFECK";
+    public static String reflectorUKWCValues = "FVPJIAOYEDRZXWGCTKUQSBNMHL";
+
+
+
     public static void main (){
 
 
